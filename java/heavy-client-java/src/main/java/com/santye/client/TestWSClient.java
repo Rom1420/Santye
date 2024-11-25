d171a0673c87542048920b75aa65d341c7ddc47f
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-11-25 17:16:31 +0100

    Feat : crete the java client ui

--- java/heavy-client-java/src/main/java/com/santye/client/TestWSClient.java
+++ java/heavy-client-java/src/main/java/com/santye/client/TestWSClient.java
@@ -7,7 +7,7 @@ import javax.xml.namespace.QName;
 
 public class TestWSClient {
 
-    public static void main(String[] args) {
+    public static String findItinerary(String departure, String destination) {
         try {
             // Créer une instance du service ItineraryService
             ItineraryService service = new ItineraryService();
@@ -15,33 +15,36 @@ public class TestWSClient {
 
             // Créer l'objet de requête ItineraryRequest
             ItineraryRequest request = new ItineraryRequest();
-            JAXBElement<String> departure = new JAXBElement<>(
+            JAXBElement<String> departureElement = new JAXBElement<>(
                     new QName("http://schemas.datacontract.org/2004/07/ConsoleApp_for_Self_Hosted_WS", "Departure"),
                     String.class,
-                    "48.8566, 2.3522"
+                    departure
             );
-            JAXBElement<String> destination = new JAXBElement<>(
+            JAXBElement<String> destinationElement = new JAXBElement<>(
                     new QName("http://schemas.datacontract.org/2004/07/ConsoleApp_for_Self_Hosted_WS", "Destination"),
                     String.class,
-                    "51.5074, -0.1278"
+                    destination
             );
 
-            request.setDeparture(departure);
-            request.setDestination(destination);
+            request.setDeparture(departureElement);
+            request.setDestination(destinationElement);
 
             // Appeler l'opération FindItinerary du service
             Itinerary itinerary = port.findItinerary(request);
 
-            // Afficher les résultats de l'itinéraire
+            // Préparer le résultat
             if (itinerary != null && itinerary.getSteps() != null) {
+                StringBuilder resultBuilder = new StringBuilder("Itinéraire trouvé :\n");
                 itinerary.getSteps().getValue().getStep().forEach(step -> {
-                    System.out.println("Step: " + step.getInstruction().getValue());
+                    resultBuilder.append("Étape : ").append(step.getInstruction().getValue()).append("\n");
                 });
+                return resultBuilder.toString();
             } else {
-                System.out.println("Aucun itinéraire trouvé.");
+                return "Aucun itinéraire trouvé.";
             }
         } catch (Exception e) {
             e.printStackTrace();
+            return "Erreur lors de la récupération de l'itinéraire : " + e.getMessage();
         }
     }
 }
