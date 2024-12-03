ae7bf5a4d9154040765f9d80eeeefec0f41f46b5
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-12-03 16:46:33 +0100

    cache

--- backend/ConsoleApp_for_Self_Hosted_WS/ConsoleApp_for_Self_Hosted_WS/Program.cs
+++ backend/ConsoleApp_for_Self_Hosted_WS/ConsoleApp_for_Self_Hosted_WS/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using ConsoleApp_for_Self_Hosted_WS.Services;
@@ -10,49 +9,20 @@ namespace ConsoleApp_for_Self_Hosted_WS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Configuration file loaded: {AppDomain.CurrentDomain.SetupInformation.ConfigurationFile}");
-
             // Créer une URI pour le service ItineraryService
             Uri httpUrl = new Uri("http://localhost:8081/api/ItineraryService");
 
             // Créer un ServiceHost pour l'ItineraryService
             ServiceHost host = new ServiceHost(typeof(ItineraryService), httpUrl);
 
-            // Configurer une liaison personnalisée
-            WebHttpBinding binding = new WebHttpBinding
-            {
-                MaxReceivedMessageSize = 2147483647,
-                MaxBufferSize = 2147483647,
-                ReaderQuotas = new System.Xml.XmlDictionaryReaderQuotas
-                {
-                    MaxDepth = 32,
-                    MaxStringContentLength = 2147483647,
-                    MaxArrayLength = 2147483647,
-                    MaxBytesPerRead = 4096,
-                    MaxNameTableCharCount = 2147483647
-                }
-            };
-
-            // Ajouter un point de terminaison avec cette liaison personnalisée
-            host.AddServiceEndpoint(typeof(IItineraryService), binding, "");
-             
-            // Ajouter un comportement RESTful
-            WebHttpBehavior webHttpBehavior = new WebHttpBehavior();
-            host.Description.Endpoints[0].Behaviors.Add(webHttpBehavior);
+            // Ajouter un point de terminaison RESTful
+            host.AddServiceEndpoint(typeof(IItineraryService), new WebHttpBinding(), "")
+                .Behaviors.Add(new WebHttpBehavior());
 
             // Activer l'échange de métadonnées
-            ServiceMetadataBehavior smb = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
-
-            if (smb == null)
-            {
-                smb = new ServiceMetadataBehavior();
-                smb.HttpGetEnabled = true;
-                host.Description.Behaviors.Add(smb);
-            }
-            else
-            {
-                smb.HttpGetEnabled = true; // Configurez l'instance existante
-            }
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            host.Description.Behaviors.Add(smb);
 
             // Démarrer le service
             host.Open();
---
+++ backend/ConsoleApp_for_Self_Hosted_WS/ConsoleApp_for_Self_Hosted_WS/Services/ProxyService.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleApp_for_Self_Hosted_WS.Models;
+using System;
+using System.Runtime.Caching;
+using ConsoleApp_for_Self_Hosted_WS.services.ConsoleApp_for_Self_Hosted_WS.Services;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_for_Self_Hosted_WS.Services
+{
+    public class ProxyService : IProxyService
+    {
+        private static readonly OpenRouteServiceClient _openRouteServiceClient = new OpenRouteServiceClient();
+        private static readonly GenericCache<Itinerary> _cache = new GenericCache<Itinerary>();
+
+        public async Task<Itinerary> GetItinerary(string departure, string destination)
+        {
+            string cacheKey = $"{departure}-{destination}";
+
+            // Vérifier si l'itinéraire est déjà en cache
+            var cachedItinerary = _cache.Get(cacheKey);
+            if (cachedItinerary != null)
+            {
+                Console.WriteLine("Using cached data");
+                return cachedItinerary;
+            }
+
+            // Si l'itinéraire n'est pas en cache, utiliser le client pour obtenir l'itinéraire
+            Console.WriteLine("Using CalculateItinerary departure/destination : "+$"{departure}/{destination}");
+            var itinerary = await _openRouteServiceClient.CalculateItinerary(departure, destination);
+
+            // Ajouter au cache avec une durée d'expiration par défaut
+            _cache.Add(cacheKey, itinerary, DateTimeOffset.Now.AddMinutes(5));
+
+            return itinerary;
+        }
+    }
+}   
